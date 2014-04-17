05845ae3e8578214f60545246c082304e5b35798
Qihan Xu <qihan.xu@gmail.com>
2014-04-17 00:07:47 -0400

    Initialization of nodes

--- Assets/Scripts/GraphNode.cs
+++ Assets/Scripts/GraphNode.cs
@@ -5,50 +5,52 @@ using System.Linq;
 
 public class GraphNode
 {
-	public int x1, y1, x2, y2;
-	public bool isVisited = false;
-	public bool isTail = false;
-	public List<GraphNode> neighbors = new List<GraphNode> ();
+		public int x1, y1, x2, y2;
+		public bool isVisited = false;
+		public bool isTail = false;
+		public List<GraphNode> neighbors = new List<GraphNode> ();
+		public GraphNode prev = null;
 	
-	public GraphNode ()
-	{
-		this.x1 = 0;
-		this.y1 = 0;
-		this.x2 = 0;
-		this.y2 = 0;
-	}
-	public Vector3 Pos(GameObject floor)
-	{
-		float posX=0, posY=0;
-		if (x1 == x2 && y2 - y1 == 1) {
-			posX = SpaceState.TileSize.x * x1 + SpaceState.TileSize.x / 2.0f + floor.collider.bounds.min.x;
-			posY = SpaceState.TileSize.y * y1 + SpaceState.TileSize.y + floor.collider.bounds.min.z;
+		public GraphNode ()
+		{
+				this.x1 = 0;
+				this.y1 = 0;
+				this.x2 = 0;
+				this.y2 = 0;
 		}
-		if (y1 == y2 && x2 - x1 == 1) {
-			posX = SpaceState.TileSize.x * x2 + floor.collider.bounds.min.x;
-			posY = SpaceState.TileSize.y * y1 + SpaceState.TileSize.y / 2.0f + floor.collider.bounds.min.z;
-		}
-		return new Vector3 (posX, 0f, posY);
-	}
 
-	public GraphNode (int i1, int j1, int i2, int j2)
-	{
-		this.x1 = i1;
-		this.y1 = j1;
-		this.x2 = i2;
-		this.y2 = j2;
-	}
+		public Vector3 Pos (GameObject floor)
+		{
+				float posX = 0, posY = 0;
+				if (x1 == x2 && y2 - y1 == 1) {
+						posX = SpaceState.TileSize.x * x1 + SpaceState.TileSize.x / 2.0f + floor.collider.bounds.min.x;
+						posY = SpaceState.TileSize.y * y1 + SpaceState.TileSize.y + floor.collider.bounds.min.z;
+				}
+				if (y1 == y2 && x2 - x1 == 1) {
+						posX = SpaceState.TileSize.x * x2 + floor.collider.bounds.min.x;
+						posY = SpaceState.TileSize.y * y1 + SpaceState.TileSize.y / 2.0f + floor.collider.bounds.min.z;
+				}
+				return new Vector3 (posX, 0f, posY);
+		}
+
+		public GraphNode (int i1, int j1, int i2, int j2)
+		{
+				this.x1 = i1;
+				this.y1 = j1;
+				this.x2 = i2;
+				this.y2 = j2;
+		}
 	
-	public void setIndice (int i1, int j1, int i2, int j2)
-	{
-		this.x1 = i1;
-		this.y1 = j1;
-		this.x2 = i2;
-		this.y2 = j2;
-	}
+		public void setIndice (int i1, int j1, int i2, int j2)
+		{
+				this.x1 = i1;
+				this.y1 = j1;
+				this.x2 = i2;
+				this.y2 = j2;
+		}
 	
-	public int distance (GraphNode gn)
-	{
-		return (Mathf.Abs (gn.x1 - this.x1) + Mathf.Abs (gn.y1 - this.y1) + Mathf.Abs (gn.x2 - this.x2) + Mathf.Abs (gn.y2 - this.y2)) / 2;	
-	}
+		public int distance (GraphNode gn)
+		{
+				return (Mathf.Abs (gn.x1 - this.x1) + Mathf.Abs (gn.y1 - this.y1) + Mathf.Abs (gn.x2 - this.x2) + Mathf.Abs (gn.y2 - this.y2)) / 2;	
+		}
 }
