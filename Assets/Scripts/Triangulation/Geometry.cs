2c791d991c63bada9d3da2007d4395ed21311c96
TontonTremblay <tonton.tremblay@gmail.com>
2014-06-11 12:48:13 -0400

    Changes

--- Assets/Scripts/Triangulation/Geometry.cs
+++ Assets/Scripts/Triangulation/Geometry.cs
@@ -10,4 +10,12 @@ public class Geometry
 	//public Vector3 [] vertex = new Vector3[4]; 
 	public List<Line> edges = new List<Line> ();
 	
+
+
+	public void DrawGeometry(GameObject parent)
+	{
+		Color c = new Color(UnityEngine.Random.Range(0.0f,1.0f),
+                           UnityEngine.Random.Range(0.0f,1.0f),
+                           UnityEngine.Random.Range(0.0f,1.0f)) ;
+	}
 }
