609b152e3971bc7105358cbf0514ff02068221ca
NavDhaliwal <navjot.tu@gmail.com>
2014-09-22 14:44:12 -0400

    First Checking:Simple map and main file

---
+++ Assets/Scripts/Waypoints/Visibility1.cs
@@ -0,0 +1,144 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Visibility1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		getObstacleEdges ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	public void getObstacleEdges()
+	{
+		//Compute one step of the discritzation
+		//Find this is the view
+		GameObject floor = (GameObject)GameObject.Find ("Floor");
+		
+		Vector3 [] vertex = new Vector3[4]; 
+		
+		//First geometry is the outer one
+		List<Geometry> geos = new List<Geometry> ();
+		
+		
+		//Drawing lines
+		//VectorLine.SetCamera3D(Camera.current); 
+		
+		//Floor
+		Vector3[] f = new Vector3[4];
+		MeshFilter mesh = (MeshFilter)(floor.GetComponent ("MeshFilter"));
+		Vector3[] t = mesh.sharedMesh.vertices; 
+		
+		Geometry tempGeometry = new Geometry (); 
+		
+		
+		vertex [0] = mesh.transform.TransformPoint (t [0]);
+		vertex [2] = mesh.transform.TransformPoint (t [120]);
+		vertex [1] = mesh.transform.TransformPoint (t [110]);
+		vertex [3] = mesh.transform.TransformPoint (t [10]);
+		
+		vertex [0].y = 1; 
+		vertex [1].y = 1; 
+		vertex [2].y = 1; 
+		vertex [3].y = 1; 
+		//these were in tempGeometry previously
+		
+		//Disabled Temporarily - Find a way to avoid floor when checking for obstacle collision
+		//geos.Add (tempGeometry);
+		
+		Vector3 [] mapBoundary = new Vector3[4]; //the map's four corners
+		
+		for (int i = 0; i < 4; i++) {
+			mapBoundary [i] = vertex [i];
+		}
+		
+		Geometry mapBG = new Geometry (); 
+		for (int i = 0; i < 4; i++)
+			mapBG.edges.Add( new Line( mapBoundary[i], mapBoundary[(i + 1) % 4]) );
+		//mapBG.DrawVertex (GameObject.Find ("temp"));
+		//mapBG.DrawGeometry(GameObject.find);
+		
+		GameObject[] obs = GameObject.FindGameObjectsWithTag ("Obs");
+		if(obs == null)
+		{
+			//Debug.Log("Add tag geos to the geometries"); 
+			return; 
+		}
+		//data holder
+		Triangulation triangulation = GameObject.Find ("Triangulation").GetComponent<Triangulation> (); 
+		triangulation.points.Clear ();
+		triangulation.colours.Clear (); 
+		
+		//Only one geometry for now
+		
+		foreach (GameObject o in obs) {
+			mesh = (MeshFilter)(o.GetComponent ("MeshFilter"));
+			t = mesh.sharedMesh.vertices; 
+			tempGeometry = new Geometry();
+			
+			vertex [0] = mesh.transform.TransformPoint (t [6]);
+			vertex [1] = mesh.transform.TransformPoint (t [8]);
+			vertex [3] = mesh.transform.TransformPoint (t [7]);
+			vertex [2] = mesh.transform.TransformPoint (t [9]);
+			
+			vertex [0].y = 1;
+			vertex [2].y = 1;
+			vertex [1].y = 1;
+			vertex [3].y = 1;
+			for (int i = 0; i< vertex.Length; i+=1) {
+				if (i < vertex.Length - 1)
+					tempGeometry.edges.Add (new Line (vertex [i], vertex [i + 1]));
+				else 	       
+					tempGeometry.edges.Add (new Line (vertex [0], vertex [i]));
+			}	
+			geos.Add (tempGeometry); 
+		}
+		
+		//lines are defined by all the points in  obs
+		lines = new List<Line> ();
+		
+		obsGeos.Clear ();
+		foreach (Geometry g in geos) {
+			obsGeos.Add(g);
+		}
+		
+		
+		//Create empty GameObject
+		GameObject temp = GameObject.Find("temp");
+		DestroyImmediate(temp);
+		temp = new GameObject("temp");
+		//CODESPACE
+		//Merging Polygons
+		for (int i = 0; i < obsGeos.Count; i++) {
+			for (int j = i + 1; j < obsGeos.Count; j++) {
+				//check all line intersections
+				if( obsGeos[i].GeometryIntersect( obsGeos[j] ) ){
+					//Debug.Log("Geometries Intersect: " + i + " " + j);
+					Geometry tmpG = obsGeos[i].GeometryMerge( obsGeos[j] ); 
+					//remove item at position i, decrement i since it will be increment in the next step, break
+					obsGeos.RemoveAt(j);
+					obsGeos.RemoveAt(i);
+					obsGeos.Add(tmpG);
+					i--;
+					break;
+				}
+			}
+		}
+		//		mapBG.DrawGeometry (temp);
+		
+		List<Geometry> finalPoly = new List<Geometry> ();//Contains all polygons that are fully insde the map
+		foreach ( Geometry g in obsGeos ) {
+			if( mapBG.GeometryIntersect( g ) && !mapBG.GeometryInside( g ) ){
+				mapBG = mapBG.GeometryMergeInner( g );
+				mapBG.BoundGeometry( mapBoundary );
+			}
+			else
+				finalPoly.Add(g);
+		}
+	}
+}
