e0a6d76ed5083fbf91b6118f7f927c24c502c54c
Abracazebra <alexbx@yandex.ru>
2015-01-26 17:47:54 -0500

    More Enemy Code added

--- Assets/Scripts/EnemyGeo.cs
+++ Assets/Scripts/EnemyGeo.cs
@@ -8,15 +8,17 @@ using Common;
 namespace Objects {
 	public class EnemyGeo : MonoBehaviour {
 		
-		public Waypoint target;
-		public float moveSpeed;
-		public float rotationSpeed;
+		//public WaypointGeo target;
+		//public float moveSpeed;
+		//public float rotationSpeed;
 		public float fovAngle = 33;
 		public float fovDistance = 5;
 		public float radius = 0.5f;
-		public float dps = 2;
-		public float maxHealth = 100;
+		//public float dps = 2;
+		//public float maxHealth = 100;
 		// The first index is always the time span you want to peek
+
+		/*
 		[HideInInspector]
 		public Vector3[] positions;
 		[HideInInspector]
@@ -28,24 +30,97 @@ namespace Objects {
 		private Vector3 dummyPosition;
 		private Quaternion dummyRotation;
 		//
+		*/
 		private Vector3 initialPosition;
-		private Quaternion initialRotation;
-		private Waypoint initialTarget;
+		private Vector3 initialForward;
+		//private Quaternion initialRotation;
+		private WaypointGeo initialTarget;
 		//
-		private Vector3 currentPosition;
-		private Quaternion currentRotation;
+		//private Vector3 currentPosition;
+		//private Quaternion currentRotation;
 		public Color LineForFOV = new Color (1.0f, 0.3f, 0.0f, 1f);
 
 
 
 
-		public Vector2 getPosition(int t){
-			return Vector2.one;
+		public Vector3 getPosition(int t){
+			computeAtTime(t);
+
+			return posT;
 		}
-		public Vector2 getForward(int t){
-			return Vector2.one;
+		public Vector3 getForward(int t){
+			computeAtTime(t);
+
+			return forT;
 		}
 
+		private int curT;
+		private Vector3 posT;
+		private Vector3 forT;
+
+		int timeLeft;
+		WaypointGeo nextWay;
+		Vector3 pos;
+		Vector3 forw;
+		
+		
+		private void computeAtTime(int t){
+			if(t != curT){
+				curT = t;
+				timeLeft = t;
+				nextWay = initialTarget;
+				pos = initialPosition;
+				forw = initialForward;
+
+				while(timeLeft > 0){
+
+					switch (nextWay.type)
+					{
+					case "wait" :
+						timeLeft = timeLeft - nextWay.waitTime;
+						break;
+					case "move" :
+						tryMove(nextWay);
+						break;
+					case "rot" :
+						tryRot(nextWay);
+						break;
+					default:
+						Debug.LogError("UNIDENTIFIED WAYPOINT FOUND, EVERTHING WILL NOW BREAK");
+						break;
+					}
+
+					nextWay = nextWay.next;
+				}
+					 
+
+			}
+		}
+
+		private void tryMove(WaypointGeo trgt){
+			float distance = Vector3.Distance(pos, trgt.transform.position);
+			int steps = Mathf.CeilToInt(distance / trgt.movSpeed);
+			if(steps > timeLeft){
+				pos = Vector3.Lerp(pos, trgt.transform.position, ((float)steps) / ((float)timeLeft));
+			}
+			else{
+				timeLeft = timeLeft - steps;
+				pos = trgt.transform.position;
+			}
+		}
+
+		private void tryRot(WaypointGeo trgt){
+					float angleDif = Vector3.Angle(forw, trgt.transform.forward);
+					int steps = Mathf.CeilToInt(angleDif / trgt.rotSpeed);
+					if(steps > timeLeft){
+						Vector3.Slerp(forw, trgt.transform.forward, ((float)steps) / ((float)timeLeft));
+					}
+					else{
+						timeLeft = timeLeft - steps;
+						forw = trgt.transform.forward;
+					}
+		}
+/*
 		// This moves the enemy in the game running environment
 		void Update () {
 			Vector3 outPos;
@@ -115,7 +190,8 @@ namespace Objects {
 		public Quaternion GetSimulatedRotation () {
 			return currentRotation;
 		}
-		
+
+*/
 
 	}
 }
---
+++ Assets/Scripts/WaypointGeo.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace Objects {
+	public class WaypointGeo : MonoBehaviour {
+		
+		public WaypointGeo next;
+		//types of waypoints will be wait, move, and rotate.
+		//thus you will do that thing, at the given speed until you match the objects stuff.
+		public string type = "wait";
+		public float movSpeed = 1;
+		public float rotSpeed = 1;
+		public int waitTime = 1;
+
+		public static bool debug = true;
+		
+		// Use this for initialization
+		void Start () {
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		}
+		
+		void OnDrawGizmos () {
+			Gizmos.color = Color.white;
+			if (debug)
+				Gizmos.DrawSphere (transform.position, 0.1f);
+		}
+	}
+}
+
