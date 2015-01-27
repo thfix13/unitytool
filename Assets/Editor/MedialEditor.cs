98903535aad6afd5fd753a298a0872f17866c7d8
Dhirendra Singh <dhiruregister@gmail.com>
2015-01-27 04:19:12 -0500

    Path development using Dijkstra.... on the phase of projection on 2D plane

---
+++ Assets/Editor/MedialEditor.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(CreateLayeredMesh))]
+public class  MedialEditor: Editor {
+	int sg=-1, sgnew;
+	GameObject g=null;
+	CreateLayeredMesh obj;
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		if(g==null)
+		{
+			g = GameObject.Find("Medial");
+			obj=g.GetComponent<CreateLayeredMesh>();
+		}
+		var text = new string[] {"Moving_gaurd", "Convex", "Arena"};
+		sgnew=GUILayout.SelectionGrid(sg, text, 1, EditorStyles.radioButton);
+		if(sgnew!=sg){
+			obj.buildArena(sgnew);
+			sg=sgnew;
+		}
+		if(GUILayout.Button("Get Medial Skeleton"))
+		{
+			
+//			obj.BuildMedial();
+		}
+//		if(GUILayout.Button("Get Medial Skeleton"))
+//		{
+//			
+//			obj.BuildMedial();
+//		}
+	}
+}
+[CustomEditor(typeof(Moving_guard_arena))]
+public class  MedialEditor2: Editor {
+	GameObject g=null;
+	Moving_guard_arena obj;
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+		if(g==null)
+		{
+			g = GameObject.Find("Medial");
+			obj=g.GetComponent<Moving_guard_arena>();
+		}
+
+		if(GUILayout.Button("Get Medial Skeleton"))
+		{
+			
+			obj.BuildMedial();
+		}
+		if(GUILayout.Button("Get Path"))
+		{
+			
+			obj.BuildPath();
+		}
+		GUILayout.BeginHorizontal("box");
+		if(GUILayout.Button("Project",GUILayout.Width(100)))
+		{
+			
+			obj.Projectpath();
+		}
+		if(GUILayout.Button("Next",GUILayout.Width(100)))
+		{
+			
+			obj.incrementt();
+		}
+		GUILayout.EndHorizontal();
+	}
+}
