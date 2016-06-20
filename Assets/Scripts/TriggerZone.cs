cb4ec5e43692e302ca8d61cd77fb0ca468fe3d13
thfix13 <thfix13@gmail.com>
2016-06-20 15:18:59 -0400

    Sound Triggers Added
    Code for sound trigger zones added; they are currently represented by
    just an x and y position and a radius.

---
+++ Assets/Scripts/TriggerZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System.Xml.Serialization;
+using System;
+
+namespace Common {
+    [Serializable]
+    public class TriggerZone {
+        public float x, y;
+        public float radius;
+    }
+}
